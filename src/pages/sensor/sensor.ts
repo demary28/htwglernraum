7894a25c3c2e43e7b1d959818970165c08d20694
Marius Rogg <rogg-marius@gmx.de>
2018-05-16 14:23:21 +0100

    added recommendation

--- src/pages/sensor/sensor.ts
+++ src/pages/sensor/sensor.ts
@@ -10,11 +10,12 @@ import { NAV } from 'ionic-angular/navigation/nav-util';
 })
 export class SensorDataPage {
 
-  private RoomID : String =  "Please choose a room";
+  private RoomID : String =  "Bitte wähle einen Raum aus";
   private Presence : String;
-  private Air: String;
-  private Noise : String;
+  private Air: Number;
+  private Noise : Number;
   public aktualisiert: Boolean = false;
+  private bewertung: String;
 
   constructor(public navCtrl: NavController, 
               public events:Events, 
@@ -27,19 +28,43 @@ export class SensorDataPage {
     this.Air = navParams.get("Air");
     this.Noise = navParams.get("Noise");
     this.aktualisiert = navParams.get("Actu");
+    this.setBewertung();
     
     //events.subscribe('setRoomID', (id, presence, air, noise) => {
     //  this.setRoom(id, presence, air, noise);
     //})
   }
 
+  setBewertung(){
+    switch (true){
+      case (this.Presence=="not detected" && this.Air<=1400 && this.Noise<=62):{
+        this.bewertung = "Room seems to be free. Recommendation: Take this room";
+        break;
+      }
+      case (this.Presence=="not detected" && this.Air<=1400 && this.Noise>=62):{
+        this.bewertung = "Room seems to be very noisy. Maybe there are roadworks nearbby. Recommendation: Look for another room";
+        break;
+      }
+      case (this.Presence=="detected" && this.Air<=1400 && this.Noise<=62):{
+        this.bewertung = "Seems that only a few persons are woking in this room quietly. Recommendation: Take this room";
+        break;
+      }
+      case (this.Presence=="detected" && this.Air<=1400 && this.Noise<=62):{
+        this.bewertung = "Seems that many persons are woking in this room, so the Air is bad. Recommendation: Look for another room";
+        break;
+      }
+      case (this.Presence=="detected" && this.Air<=1000 && this.Noise>=62):{
+        this.bewertung = "Seems that either many people are working in that room with open window or few people loudly. Recommendation: Give it a try, when you have headphones :)";
+        break;
+      }
+      case (this.Presence=="detected" && this.Air>=1000 && this.Noise>=62):{
+        this.bewertung = "Seems that the persons in this room are woking loud. Recommendation: Look for another room";
+        break;
+      }
 
-  public setRoom(id:String, presence:String, air:String, noise: String){
-    this.RoomID = id;
-    this.Presence = presence;
-    this.Air = air;
-    this.Noise = noise;
-    this.aktualisiert = true;
+    }
+  
+
   }
 
   showFullImage(img, title){
